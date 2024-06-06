53ff54d5af924e03523efad5d92e6b4a231a85a8
Juan <juancq@gmail.com>
2024-06-06 19:35:35 +1000

    Rewrite training to use pytorch lightning

--- train_model.py
+++ train_model.py
@@ -1,115 +1,72 @@
 import os
 import torch
 import numpy as np
+import polars as pl
 import random
 import pickle
-from tqdm import tqdm
-from model import HALOModel
+from pathlib import Path
+from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from halo_data_module import HALODataModule
+from lightning.pytorch.callbacks import RichProgressBar
+
+from halo import HALOModel
 from config import HALOConfig
 
-SEED = 4
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-config = HALOConfig()
+def main():
+    SEED = 4
+    seed_everything(SEED)
+    torch.set_float32_matmul_precision('high')
 
-local_rank = -1
-fp16 = False
-if local_rank == -1:
-  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-  n_gpu = torch.cuda.device_count()
-else:
-  torch.cuda.set_device(local_rank)
-  device = torch.device("cuda", local_rank)
-  n_gpu = 1
-  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-  torch.distributed.init_process_group(backend='nccl')
-if torch.cuda.is_available():
-  torch.cuda.manual_seed_all(SEED)
+    dv = pl.read_excel("df_vars_new.xlsx")
+    config = HALOConfig(dv)
 
-train_ehr_dataset = pickle.load(open('./data/trainDataset.pkl', 'rb'))
-val_ehr_dataset = pickle.load(open('./data/valDataset.pkl', 'rb'))
+    # Initialize data module
+    # set data path
+    data_path = Path('/mnt/data_volume/apdc/lumos')
+    # must implement your own data module, example provided in halo_data_module
+    data_module = HALODataModule(config, batch_size=config.batch_size, 
+                    train_name=data_path / 'df_trn_ppn.parquet',
+                    val_name=data_path / 'df_val_ppn.parquet',
+                    train_split=0.5,
+                    )
 
-def get_batch(loc, batch_size, mode):
-  # EHR data saved as [(P_1, L_1), (P_2, L_2), ... , (P_i, L_i)]
-  #   Where each patient P is [V_1, V_2, ... , V_j]
-  #     Where each visit V is [C_1, C_2, ... , C_k]
-  #   And where each Label L is a binary vector [L_1 ... L_n]
-  if mode == 'train':
-    ehr = train_ehr_dataset[loc:loc+batch_size]
-  elif mode == 'valid':
-    ehr = val_ehr_dataset[loc:loc+batch_size]
-  else:
-    ehr = test_ehr_dataset[loc:loc+batch_size]
-    
-  batch_ehr = np.zeros((len(ehr), config.n_ctx, config.total_vocab_size))
-  batch_mask = np.zeros((len(ehr), config.n_ctx, 1))
-  for i, p in enumerate(ehr):
-    visits = p['visits']
-    for j, v in enumerate(visits):
-      batch_ehr[i,j+2][v] = 1
-      batch_mask[i,j+2] = 1
-    batch_ehr[i,1,config.code_vocab_size:config.code_vocab_size+config.label_vocab_size] = np.array(p['labels']) # Set the patient labels
-    batch_ehr[i,len(visits)+1,config.code_vocab_size+config.label_vocab_size+1] = 1 # Set the final visit to have the end token
-    batch_ehr[i,len(visits)+2:,config.code_vocab_size+config.label_vocab_size+2] = 1 # Set the rest to the padded visit token
-  
-  batch_mask[:,1] = 1 # Set the mask to cover the labels
-  batch_ehr[:,0,config.code_vocab_size+config.label_vocab_size] = 1 # Set the first visits to be the start token
-  batch_mask = batch_mask[:,1:,:] # Shift the mask to match the shifted labels and predictions the model will return
-  return batch_ehr, batch_mask
+    # Initialize model
+    model = HALOModel(config)
+    model.transformer = torch.compile(model.transformer)
+    model.ehr_head = torch.compile(model.ehr_head)
 
-def shuffle_training_data(train_ehr_dataset):
-  np.random.shuffle(train_ehr_dataset)
+    # Checkpoint callback
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_loss',
+        dirpath='./save/',
+        filename='halo_model-{epoch:02d}-{val_loss:.3f}',
+        save_top_k=1,
+        mode='min',
+    )
 
-model = HALOModel(config).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
-if os.path.exists("./save/halo_model"):
-  print("Loading previous model")
-  checkpoint = torch.load('./save/halo_model', map_location=torch.device(device))
-  model.load_state_dict(checkpoint['model'])
-  optimizer.load_state_dict(checkpoint['optimizer'])
+    # Early stopping callback
+    early_stopping_callback = EarlyStopping(
+        monitor='val_loss',
+        patience=10,  # number of epochs with no improvement after which training will be stopped
+        mode='min'
+    )
 
-# Train
-global_loss = 1e10
-for e in tqdm(range(config.epoch)):
-  shuffle_training_data(train_ehr_dataset)
-  for i in range(0, len(train_ehr_dataset), config.batch_size):
-    model.train()
-    
-    batch_ehr, batch_mask = get_batch(i, config.batch_size, 'train')
-    batch_ehr = torch.tensor(batch_ehr, dtype=torch.float32).to(device)
-    batch_mask = torch.tensor(batch_mask, dtype=torch.float32).to(device)
-    
-    optimizer.zero_grad()
-    loss, _, _ = model(batch_ehr, position_ids=None, ehr_labels=batch_ehr, ehr_masks=batch_mask, pos_loss_weight=config.pos_loss_weight)
-    loss.backward()
-    optimizer.step()
-    
-    if i % (500*config.batch_size) == 0:
-      print("Epoch %d, Iter %d: Training Loss:%.6f"%(e, i, loss * 8))
-    if i % (500*config.batch_size) == 0:
-      if i == 0:
-        continue
-    
-      model.eval()
-      with torch.no_grad():
-        val_l = []
-        for v_i in range(0, len(val_ehr_dataset), config.batch_size):
-          batch_ehr, batch_mask = get_batch(v_i, config.batch_size, 'valid')
-          batch_ehr = torch.tensor(batch_ehr, dtype=torch.float32).to(device)
-          batch_mask = torch.tensor(batch_mask, dtype=torch.float32).to(device)
-  
-          val_loss, _, _ = model(batch_ehr, position_ids=None, ehr_labels=batch_ehr, ehr_masks=batch_mask, pos_loss_weight=config.pos_loss_weight)
-          val_l.append((val_loss).cpu().detach().numpy())
-          
-        cur_val_loss = np.mean(val_l)
-        print("Epoch %d Validation Loss:%.7f"%(e, cur_val_loss))
-        if cur_val_loss < global_loss:
-          global_loss = cur_val_loss
-          state = {
-                'model': model.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'iteration': i
-            }
-          torch.save(state, './save/halo_model')
-          print('\n------------ Save best model ------------\n')
+    # Trainer
+    trainer = Trainer(
+        max_epochs=config.epoch,
+        callbacks=[checkpoint_callback, early_stopping_callback,
+                RichProgressBar(leave=True)],
+        # add mlflow here
+        #logger=True,
+    )
+
+    # Train the model
+    trainer.fit(model, data_module)
+
+    # Evaluate the model
+    trainer.test(model, datamodule=data_module)
+
+
+if __name__ == '__main__':
+    main()
